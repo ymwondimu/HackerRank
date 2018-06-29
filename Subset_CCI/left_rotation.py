fbd97fe168ca8cb47f4cf078cd9b72a05e6eabce
Yeab Wondimu <ywondimu6@gatech.edu>
2018-06-29 16:32:01 -0700

    Completed left_rotation problem

--- Subset_CCI/left_rotation.py
+++ Subset_CCI/left_rotation.py
@@ -1,2 +1,17 @@
 def main():
-    pass
+    left_rotation(3, [1,2,3,4,5])
+
+def left_rotation(d, arr):
+    size = len(arr)
+    n = d%size
+    back = arr[n:]
+    front = arr[:n]
+
+    arr[:n] = back
+    arr[(size-n):] = front
+
+    print (" ".join(str(x) for x in arr))
+    return arr
+
+if __name__ == "__main__":
+    main()
